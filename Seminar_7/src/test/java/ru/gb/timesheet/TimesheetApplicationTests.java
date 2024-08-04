533f8c056a4e0c53533e9cf4c04348244e1a8d8c
Saikala <s.borubaevaa@gmail.com>
2024-08-05 00:11:37 +0600

    first commit hw#7

---
+++ Seminar_7/src/test/java/ru/gb/timesheet/TimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetApplication7Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
